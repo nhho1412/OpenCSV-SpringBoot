bcbbed320103f397c33cdd83f056b0fbdac6c9ba
Tiger <huynhho1412@outlook.com>
2021-01-20 22:36:27 +0700

    csvWrite commit

--- demoOpenCSV/src/main/java/com/tiger/demoOpenCSV/controller/OpenCsvController.java
+++ demoOpenCSV/src/main/java/com/tiger/demoOpenCSV/controller/OpenCsvController.java
@@ -1,5 +1,24 @@
 package com.tiger.demoOpenCSV.controller;
 
-public class OpenCsvController {
+import java.io.IOException;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tiger.demoOpenCSV.service.OpenCsvWriter;
+
+@RestController
+@RequestMapping(value = "/tiger-openCSV")
+public class OpenCsvController {
+	@Autowired
+	OpenCsvWriter openCsvWriter;
+	
+	@GetMapping(value = "/demoCSV")
+	public String demoCSV(@RequestParam("title") String title) throws IOException {
+		openCsvWriter.csvWriter();
+		return "done";
+	}
 }
